97a2902db5ed44700ffb0b240eee1bab71b4c5da
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-10-28 14:04:21 -0700

    render todo list, form and buttons / added functionality to buttons, added completed toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
 
+import data from './components/TodoComponents/data';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: data
+    };
+  }
+
+  addTodo = task => {
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
+  toggleCompleted = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id ) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        } else {
+          return todo;
+        }
+      })
+    });
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1> React Todo List! </h1>
+        <TodoList 
+          todos={this.state.todos} 
+          toggleCompleted={this.toggleCompleted}
+        />
+        <TodoForm addTodo={this.addTodo} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export default class TodoForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            newTodo: '',
+        };
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            newTodo: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log("it's working!");
+        this.props.addTodo(this.state.newTodo);
+        //reset form after submit
+        this.setState({newTodo: ''})
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type='text'
+                        name='todo'
+                        id='todo'
+                        value={this.state.newTodo}
+                        onChange={this.handleInputChange}
+                    />
+                    <button className='add-btn'>Add Todo:</button>
+                    <button 
+                        className='clear-btn' 
+                        onClick={this.props.clearCompleted}>Clear Completed:</button>
+                </form>
+                
+            </div>
+        )
+    }
+}
